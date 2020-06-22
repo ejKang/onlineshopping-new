0cccc4561cb17cfeaa99f4e903c9be33f5f3e5ea
ejKang <joomn11@gmail.com>
2020-06-22 23:46:57 +0900

    add comments in springBoot main

--- onlineshopping/src/main/java/net/ejkang/onlineshopping/DemoApplication.java
+++ onlineshopping/src/main/java/net/ejkang/onlineshopping/DemoApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-@ComponentScan(basePackages = {"net.ejkang"})
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class) // class not found exception
+@ComponentScan(basePackages = {"net.ejkang"}) // shoppingbackend project not found exception
 public class DemoApplication {
 
 	public static void main(String[] args) {
